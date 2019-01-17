17d01042c0281381e64461e935f0f93d71a59302
zac <suoxd123@126.com>
2019-01-17 23:26:53 +0800

    Site updated: 2019-01-17 23:26:44

--- js/mobile.js
+++ js/mobile.js
@@ -19,14 +19,14 @@ define([], function(){
             document.getElementById("js-mobile-tagcloud").innerHTML = $tag.innerHTML;
         }
         if($aboutme){
-            document.getElementById("js-mobile-aboutme").innerHTML = $aboutme.innerHTML;
+//            document.getElementById("js-mobile-aboutme").innerHTML = $aboutme.innerHTML;
         }
         if($friends){
             document.getElementById("js-mobile-friends").innerHTML = $friends.innerHTML;
         }
         if($topics)
         {
-            document.getElementById("js-mobile-topics").innerHTML = $topics.innerHTML;
+            document.getElementById("js-mobile-topics").innerHTML = $topics.innerHTML.replace(/topic-node/g,"topic-node-mobile");
         }
     }
     //第三步 -- 根据数据渲染DOM
@@ -46,8 +46,8 @@ define([], function(){
         var tagStr = $tag?'<span class="viewer-title">'+ menuList("tags") + '</span><div class="viewer-div tagcloud" id="js-mobile-tagcloud"></div>':"";
         var friendsStr = $friends?'<span class="viewer-title">'+ menuList("friends") + '</span><div class="viewer-div friends" id="js-mobile-friends"></div>':"";
         var aboutmeStr = $aboutme?'<span class="viewer-title">'+ menuList("about") + '</span><div class="viewer-div aboutme" id="js-mobile-aboutme"></div>':"";
-        var topicsStr = "";
-        if($topics != null){
+        var topicsStr = '<div class="viewer-div topics" id="js-mobile-topics"></div>';
+/*        if($topics != null){
             var iCount = $topics.children.length;
             for(var i=0;i<iCount;i++){
                 var tmpItem = $topics.children[i].children[0];
@@ -60,10 +60,10 @@ define([], function(){
             }
             topicsStr += '<div class="viewer-div topics" id="js-mobile-topics"></div>'
         }
-
+*/
         $viewer.innerHTML = '<div id="viewer-box">\
         <div class="viewer-box-l">\
-            <div class="viewer-box-wrap">'+aboutmeStr+friendsStr+tagStr+topicsStr+'</div>\
+            <div class="viewer-box-wrap">'+topicsStr+'</div>\
         </div>\
         <div class="viewer-box-r"></div>\
         </div>';
